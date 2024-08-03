73bba126ff62752cda0016c8ffbfac2809605909
Samir <bettahar.samir@outlook.com>
2024-08-03 20:31:11 +0100

    Initial commit
    To connect your workspace to Nx Cloud, push your repository
    to your git hosting provider and go to the following URL:
    
    https://cloud.nx.app/connect/cSMpjo9bZw

---
+++ apps/compare-react-astro-svelte/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'compare-react-astro-svelte',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/compare-react-astro-svelte',
+};
---
+++ apps/compare-react-astro-svelte/src/app/app.tsx
@@ -0,0 +1,11 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="compare-react-astro-svelte" />
+    </div>
+  );
+}
+
+export default App;
